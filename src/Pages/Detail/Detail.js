4596ed322e8c3ccc6237646ce6c03f2ca29fb9b1
BangBang <phibang7899@gmail.com>
2021-11-15 13:52:13 +0700

    push new forler

--- src/Pages/Detail/Detail.js
+++ src/Pages/Detail/Detail.js
@@ -16,9 +16,9 @@ const { TabPane } = Tabs;
 export default function Detail(props) {
 	const { phimDetail } = useSelector((state) => state.QuanLyPhimReducer);
 	const [tabPosition, setState] = useState("left");
-	console.log(phimDetail);
-  const dispatch = useDispatch();
- 
+	
+	const dispatch = useDispatch();
+
 	useEffect(() => {
 		//lấy thông tin param từ url
 		let { id } = props.match.params;
@@ -61,24 +61,17 @@ export default function Detail(props) {
 											left: "5%",
 										}}
 									>
-										<div>
+										<div className="mb-10">
 											<span className="text-xs font-medium">
 												{moment(phimDetail.ngayKhoiChieu).format("yyyy-MM-DD ")}
 											</span>
 										</div>
-										<div>
+										<div className="mb-20">
 											<p className="text-4xl font-medium">
 												{phimDetail.tenPhim}
 											</p>
 										</div>
-										<div>
-											<p className="text-lg font-medium">
-												{" "}
-												{phimDetail.moTa?.length > 200
-													? phimDetail.moTa.slice(0, 200) + "..."
-													: phimDetail.moTa}
-											</p>
-										</div>
+
 										<div>
 											<button
 												style={{
@@ -120,7 +113,82 @@ export default function Detail(props) {
 					</div>
 					<div>
 						<Tabs defaultActiveKey="1" centered>
-							<TabPane tab="Tab 1" key="1" style={{ backgroundColor: "white" }}>
+							<TabPane
+								tab={
+									<div>
+										<h1
+											style={{
+												color: "white",
+												fontWeight: "500",
+												fontSize: "20px",
+											}}
+										>
+											Thông Tin
+										</h1>
+									</div>
+								}
+								key="1"
+							>
+								<div className="flex text-white font-medium">
+									<div className="w-1/2">
+										<div className="flex w-full">
+											<div className="w-2/6">Tên Phim:</div>
+											<div className="w-3/6">{phimDetail.tenPhim}</div>
+										</div>
+										<div className="flex w-full my-5">
+											<div className="w-2/6">Ngày công chiếu</div>
+											<div className="w-3/6">
+												{" "}
+												{moment(phimDetail.ngayKhoiChieu).format("yyyy-MM-DD ")}
+											</div>
+										</div>
+
+										{phimDetail.dangChieu ? (
+											<div className="w-full my-5">
+												{" "}
+												<div className="w-2/6">Đang Chiếu</div>{" "}
+											</div>
+										) : (
+											<></>
+										)}
+										{phimDetail.hot ? (
+											<div className="w-full my-5">
+												{" "}
+												<div className="w-2/6">Hot</div>{" "}
+											</div>
+										) : (
+											<></>
+										)}
+										{phimDetail.sapChieu ? (
+											<div className="w-full my-5">
+												{" "}
+												<div className="w-2/6">Sắp Chiếu</div>{" "}
+											</div>
+										) : (
+											<></>
+										)}
+										<div className="flex w-full my-5">
+											<div className="w-2/6">Đáng Giá:</div>
+											<div className="w-3/6">
+												{" "}
+												<Rate
+													allowHalf
+													value={phimDetail.danhGia / 2}
+													style={{ color: "#78ed78", fontSize: 20 }}
+												/>
+											</div>
+										</div>
+									</div>
+									<div className=" w-1/2">
+										<div className="w-full">
+											<p>Nội Dung</p>
+										</div>
+										<div className="w-full">
+											<p>{phimDetail.moTa}</p>
+										</div>
+									</div>
+								</div>
+
 								<Tabs tabPosition={tabPosition}>
 									{phimDetail.heThongRapChieu?.map((rap, index) => {
 										<TabPane
@@ -133,16 +201,29 @@ export default function Detail(props) {
 												></img>
 											}
 											key={index}
-										>
-											Content of Tab 1
-										</TabPane>;
+										></TabPane>;
 									})}
 								</Tabs>
 							</TabPane>
-							<TabPane tab="Tab 2" key="2">
-                <Tabs tabPosition={tabPosition}>
-                {phimDetail.heThongRapChieu?.map((rap, index) => {
-										<TabPane
+							<TabPane
+								tab={
+									<div>
+										<h1
+											style={{
+												color: "white",
+												fontWeight: "500",
+												fontSize: "20px",
+											}}
+										>
+											Lịch Chiếu
+										</h1>
+									</div>
+								}
+								key="2"
+							>
+								<Tabs tabPosition={tabPosition} className="bg-white ">
+									{phimDetail.heThongRapChieu?.map((rap, index) => {
+									    return 	<TabPane
 											tab={
 												<img
 													src={rap.logo}
@@ -154,21 +235,16 @@ export default function Detail(props) {
 											key={index}
 										>
 											Content of Tab 1
-										</TabPane>;
+										</TabPane>
 									})}
-									<TabPane tab="Tab 1" key="1">
-										Content of Tab 1
-									</TabPane>
-									<TabPane tab="Tab 2" key="2">
+
+									{/* <TabPane tab="Tab 2" key="2">
 										Content of Tab 2
 									</TabPane>
 									<TabPane tab="Tab 3" key="3">
 										Content of Tab 3
-									</TabPane>
+									</TabPane> */}
 								</Tabs>
-							</TabPane>
-							<TabPane tab="Tab 3" key="3">
-								Content of Tab Pane 3
 							</TabPane>
 						</Tabs>
 					</div>
